76e0098be0b11504dc74488f0de133434f119b66
osmanbaskaya <obaskaya@ku.edu.tr>
2013-09-27 01:14:13 +0300

    ppl calc done

--- natural-language-processing/hw1/ppl-calc.py
+++ natural-language-processing/hw1/ppl-calc.py
@@ -6,9 +6,22 @@ __author__ = "Osman Baskaya"
 import re
 import sys
 import gzip
+import numpy as np
 
 fn = sys.argv[1]
-
 ppl_file = gzip.open(fn).read()
 
-regex = 
+
+regex = re.compile(r'\[ (-\d+\.\d+) \]\n')
+logprobs =  np.array(regex.findall(ppl_file), dtype='float64')
+total_lp = logprobs.sum()
+ppl =  np.power(10, -total_lp / logprobs.shape[0])
+
+regex_sent = re.compile(r'1 sentences, ')
+total_sent = len(regex_sent.findall(ppl_file))
+x = -total_lp / (logprobs.shape[0] - total_sent)
+ppl1 = np.power(10, x)
+print "logprobs= {}, ppl= {}, ppl1= {}".format(total_lp, ppl, ppl1)
+
+
+
