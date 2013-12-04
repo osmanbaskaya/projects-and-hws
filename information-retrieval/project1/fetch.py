f3a8e6091ba3999ff0611137e66ee6dcefe46842
osmanbaskaya <obaskaya@ku.edu.tr>
2013-12-04 17:31:36 +0200

    fetch almost done

--- information-retrieval/project1/fetch.py
+++ information-retrieval/project1/fetch.py
@@ -19,43 +19,86 @@ from urllib2 import urlopen
 import pickle
 import re
 
-
 query_file = sys.argv[1]
 stoplist_file = sys.argv[2]
-database = int(sys.argv[3])
-is_only_alpha_numeric = bool(sys.argv[4])
+stem_file = sys.argv[3]
+database = int(sys.argv[4])
+is_only_alpha_numeric = bool(sys.argv[5])
+out_file_name = sys.argv[6]
 
 BASE = "http://fiji4.ccs.neu.edu/~zerg/lemurcgi/lemur.cgi?d={}&g=p&v={}"
 
-def preprocess_query(line):
+def get_stopset():
+    return set([line.strip() for line in open(stoplist_file)])
 
-    #TODO ayni kelimeleri tekrar fetch etme!
+def get_stem_dict():
+    stem_dict = {}
+    for line in open(stem_file).readlines()[1:]:
+        line = line.replace(" | ", "").split()
+        stemmed = line[0]
+        for word in line[1:]:
+            stem_dict[word] = stemmed
+
+    return stem_dict
+
+def fetch_details_from_lemur(word):
+    url  = BASE.format(database, word)
+    page = urlopen(url)
+    page = page.read().split('\n')
+
+    ctf, df = map(int, page[8].split())
+    return [line.split() for line in page[9:9+df]]
+
+def create_pkl_file(data, fn):
     
+    pickle.dump(data, open(fn, 'w'))
+
+def preprocess_query(line, stemdict, stopset):
+
     if is_only_alpha_numeric:
         # removes all non-alphanumeric characters except whitespace
-        line = re.sub("[^a-zA-Z0-9\w]", "", line)
+        line = re.sub("[^a-zA-Z0-9\s]", "", line)
 
     line = line.split()
     query_id, line = line[0], line[1:]
+    print >> sys.stderr, "Processing: {}".format(query_id)
 
-    if database == 1:
-        pass
-    elif database == 2:
-        pass
-    else:
-        pass
-    return 
+    preprocessed_words = []
+    for word in line:
+        if stopset is not None:
+            if word in stopset:
+                continue
+        if stemdict is not None:
+            word = stemdict.get(word, word)
+        preprocessed_words.append(word)
 
-def fetch_details_from_lemur(word):
-    page = urlopen(BASE.format(database, word))
+    return query_id, preprocessed_words
 
-def create_pkl_file(data):
-    pass
-
-def process_queries():
+def process_queries(stemdict, stopset):
+    queries = {}
+    d = {}
     for line in open(query_file):
-         query_id, words = preprocess_query(line)
+        query_id, words = preprocess_query(line, stemdict, stopset)
+        queries[query_id] = words
+        for word in words:
+             if word not in d:
+                 d[word] = fetch_details_from_lemur(word)
 
+    return d, queries
 
-process_queries()
+def main():
+    stopset = None
+    stemdict =  None
+    if database == 1:
+        stemdict = get_stem_dict()
+    elif database == 2:
+        stopset = get_stopset()
+    elif database == 3:
+        stemdict = get_stem_dict()
+        stopset = get_stopset()
 
+    d, queries = process_queries(stemdict, stopset)
+    create_pkl_file(d, out_file_name)
+    create_pkl_file(queries, "queries-" + out_file_name)
+
+main()
